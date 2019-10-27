cb7e91e1d3710ffe7a6be40a3385cdad22d91b5d
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2019-10-27 20:11:28 +0100

    Adds deep pattern

--- deep/generate.py
+++ deep/generate.py
@@ -1,6 +1,6 @@
 from itertools import permutations, islice
 from string import ascii_lowercase as alphabet
-from random import choices, shuffle
+from random import choices, shuffle, randint
 
 from typing import List, Set, Dict, Tuple, Optional
 
@@ -38,11 +38,10 @@ class SymbolBuilder:
         return node
 
     def set_idents_bfs(self, idents: List):
-        queue = []
-        queue += self.childs
+        queue = deque(self.childs)
         idents_index = 0
         while len(queue) > 0:
-            node = queue.pop()
+            node = queue.popleft()
             node.ident = idents[idents_index]
             idents_index += 1
             queue += node.childs
@@ -100,6 +99,9 @@ class SymbolBuilder:
                     return True
         return False
 
+    def bfs_path(self, index: int):
+        return next(islice(self.traverse_bfs_path(), index, index + 1))[0]
+
     @property
     def symbol(self):
         return deepcopy(self.childs[0])
@@ -130,5 +132,72 @@ def create_samples_permutation(depth=2, spread=1, max_size=120):
 
         if len(samples) >= max_size:
             break
+
+    return samples, idents, classes
+
+
+def _count_pattern(sequence, pattern):
+    findings = 0
+    plen = len(pattern)
+    for i in range(len(sequence) - plen+1):
+        if sequence[i:i+plen] == pattern:
+            findings += 1
+    return findings
+
+
+def create_complex_pattern_in_noise(depth=4, spread=2, max_size=120, pattern_depth=1):
+    '''Embeds a unique and fixed pattern (beginning of the alphabet) into noise'''
+
+    if pattern_depth >= depth:
+        raise f'Pattern depth ({pattern_depth}) must be smaller than outer depth ({depth})'
+    samples = []
+
+    builder = SymbolBuilder()
+    for _ in range(depth-1):
+        builder.add_level_uniform(spread)
+
+    # Create pattern
+    idents_reservoir = generate_idents()
+    pattern_size = sum([spread**l for l in range(0, pattern_depth)])
+    pattern = list(islice(idents_reservoir, pattern_size))
+    # pattern_builder = SymbolBuilder()
+    # for _ in range(pattern_depth):
+    #     builder.add_level_uniform(spread)
+    # pattern_size = sum([spread**l for l in range(0, pattern_depth+1)])
+    # idents = list(islice(idents_reservoir, pattern_size))
+    # pattern_builder.set_idents_bfs(idents)
+    # pattern = pattern_builder.symbol
+
+    # Noise
+    idents_reservoir = generate_idents()
+
+    size = sum([spread**l for l in range(0, depth)])
+    idents = list(islice(idents_reservoir, size))
+    classes = []
+
+    max_depth = depth - pattern_depth+1
+    pos_count = pattern_size = sum(
+        [spread**l for l in range(0, max_depth)])
+
+    classes_dict = {}
+
+    while len(samples) < max_size:
+        shuffled = deepcopy(idents)
+        shuffle(shuffled)
+
+        builder.set_idents_bfs(shuffled)
+        if builder.has_pattern(pattern):
+            continue
+        # Find position
+        pos = randint(0, pos_count-1)
+        path = builder.bfs_path(pos)
+        builder.set_idents_bfs_at(pattern, path)
+
+        if pos not in classes_dict:
+            classes_dict[pos] = len(classes_dict)
+            classes.append(classes_dict[pos])
+        class_id = classes_dict[pos]
+
+        samples.append((class_id, builder.symbol))
 
     return samples, idents, classes
