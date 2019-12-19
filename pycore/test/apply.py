76fb8c38a18a916eff43522a7273ac4b3fc63b72
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2019-12-19 18:31:51 +0100

    fixup

--- pycore/test/apply.py
+++ pycore/test/apply.py
@@ -2,6 +2,8 @@
 
 from pycore import Context, Symbol, fit, apply
 
+
+# Readme example
 context = Context.standard()
 
 
@@ -9,13 +11,28 @@ def variable_creator():
     return Symbol.parse(context, "z")
 
 
+# First step
 initial = Symbol.parse(context, "b*(c*d-c*d)=e")
+# Rule
 condition = Symbol.parse(context, "a-a")
 conclusion = Symbol.parse(context, "0")
 
 mapping = fit(initial, condition)
 
+deduced = apply(mapping[0], variable_creator, initial, conclusion)
+
+# >> b*0=e
+print(f'Deduced: {deduced}')
+
+# Second step
+
+# Rule
+condition = Symbol.parse(context, "a*0")
+conclusion = Symbol.parse(context, "0")
+
+mapping = fit(deduced, condition)
 
 deduced = apply(mapping[0], variable_creator, initial, conclusion)
 
-print(deduced)
+# >> b*0=e
+print(f'Deduced: {deduced}')
