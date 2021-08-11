d9e7837f3affdbdc6e2a91957475e03bc2ab5049
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2021-08-11 10:30:00 +0200

    wip

--- pycore/src/scenario.rs
+++ pycore/src/scenario.rs
@@ -1,7 +1,11 @@
 use core::io::Scenario;
 use pyo3::exceptions;
 use pyo3::prelude::*;
+use std::collections::HashMap;
 use std::rc::Rc;
+
+use crate::context::PyContext;
+use crate::rule::PyRule;
 
 /// Python Wrapper for core::io::Scenario
 #[pyclass(name=Scenario,subclass)]
@@ -19,5 +23,46 @@ impl PyScenario {
             }),
             Err(msg) => Err(PyErr::new::<exceptions::IOError, _>(msg)),
         }
+    }
+
+    #[getter]
+    fn rules(&self) -> PyResult<HashMap<String, PyRule>> {
+        Ok(self
+            .inner
+            .rules
+            .iter()
+            .map(|(k, v)| {
+                (
+                    k.clone(),
+                    PyRule {
+                        inner: Rc::new(v.clone()),
+                    },
+                )
+            })
+            .collect())
+    }
+
+    #[getter]
+    fn problems(&self) -> PyResult<HashMap<String, PyRule>> {
+        Ok(self
+            .inner
+            .problems
+            .iter()
+            .map(|(k, v)| {
+                (
+                    k.clone(),
+                    PyRule {
+                        inner: Rc::new(v.clone()),
+                    },
+                )
+            })
+            .collect())
+    }
+
+    #[getter]
+    fn declarations(&self) -> PyResult<PyContext> {
+        Ok(PyContext {
+            inner: self.inner.declarations.clone(),
+        })
     }
 }
